3627f033fddb02b2a01e2dec1d996f503aaeb040
vimal Raveendran <vimalraveendra@gmail.com>
2020-05-12 16:30:49 +0200

    Created CountrySelector action creators & Fetched Asynchronous API

---
+++ src/Redux/CountrySelector/CountrySelector.actions.js
@@ -0,0 +1,43 @@
+import { CountrySelectorActionTypes } from "./CountrySelector.types";
+import { fetchCountryData } from "../../Api/Api";
+
+// fetching the country name whenever the user changes
+export const handleCountry = (event) => ({
+  type: CountrySelectorActionTypes.HANDLE_COUNTRY_CHANGE,
+  payload: event,
+});
+// fetching of country data started
+export const fetchedCountriesPending = () => ({
+  type: CountrySelectorActionTypes.REQUEST_COUNTRY_DATA_PENDING,
+});
+
+// fetching of country data success
+export const fetchedCountriesSuccess = (data) => {
+  console.log("data", data);
+  return {
+    type: CountrySelectorActionTypes.REQUEST_COUNTRY_DATA_SUCCESS,
+    payload: data,
+  };
+};
+
+// fetching of country data failed
+export const fetchedCountriesFailed = (error) => ({
+  type: CountrySelectorActionTypes.REQUEST_COUNTRY_DATA_FAILED,
+  payload: error,
+});
+
+// dispatching the fetchedCountries function to redux-thunk
+// redux-thunk intercept the function & invoke the function
+//whenever its gets a dispatch inside of this action function
+// then its going to dispatch the actual action to the reducer
+// first time thunk gets dispatch(fetchCountriesPending()) i.e
+//in our case the action is REQUEST_COUNTRY_DATA_PENDING & so on
+export const fetchedCountries = () => async (dispatch) => {
+  dispatch(fetchedCountriesPending());
+  try {
+    const response = await fetchCountryData();
+    dispatch(fetchedCountriesSuccess(response));
+  } catch (error) {
+    dispatch(fetchedCountriesFailed(error));
+  }
+};
