76ce68fdc35978ece760aa7902031bd7cb6bff89
vimal Raveendran <vimalraveendra@gmail.com>
2020-05-14 15:58:42 +0200

    Updated CountrySelector component to class component and connected to redux store

--- src/Components/CountrySelector/CountrySelector.jsx
+++ src/Components/CountrySelector/CountrySelector.jsx
@@ -1,48 +1,46 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React from "react";
 import styles from "./CountrySelector.module.css";
 import cx from "classnames";
 
 import { connect } from "react-redux";
-import { handleCountry } from "../../Redux/CountrySelector/CountrySelector.actions";
-
-import { fetchCountryData } from "../../Api/Api";
 import {
-  countrySelectorReducer,
-  initialState,
-} from "../../Redux/CountrySelector/CountrySelector.reducer";
+  handleCountry,
+  fetchedCountries,
+} from "../../Redux/CountrySelector/CountrySelector.actions";
 
-const CountrySelector = ({ handleCountry }) => {
-  const [selectedCountries, setSelectedCountries] = useState([]);
-  const [state, dispatch] = useReducer(countrySelectorReducer, initialState);
-
-  useEffect(() => {
-    const fetchedCountries = async () => {
-      setSelectedCountries(await fetchCountryData());
-    };
-    fetchedCountries();
-  }, [setSelectedCountries]);
-
-  return (
-    <div className={styles.container}>
-      <select
-        className={cx(styles.selector, styles.option)}
-        onChange={handleCountry}
-      >
-        <option className={styles.option} value="">
-          Select Country
-        </option>
-        {selectedCountries.map(({ name }, index) => (
-          <option key={index} value={name}>
-            {name}
+class CountrySelector extends React.Component {
+  componentDidMount() {
+    this.props.fetchedCountries(this.props.country);
+  }
+  render() {
+    const { handleCountry, selectedCountries } = this.props;
+    return (
+      <div className={styles.container}>
+        <select
+          className={cx(styles.selector, styles.option)}
+          onChange={handleCountry}
+        >
+          <option className={styles.option} value="">
+            Select Country
           </option>
-        ))}
-      </select>
-    </div>
-  );
-};
+          {selectedCountries.map(({ name }, index) => (
+            <option key={index} value={name}>
+              {name}
+            </option>
+          ))}
+        </select>
+      </div>
+    );
+  }
+}
 
+const mapStateToProps = ({ countries: { selectedCountries, country } }) => ({
+  selectedCountries,
+  country,
+});
 const mapDispatchToProps = (dispatch) => ({
   handleCountry: (e) => dispatch(handleCountry(e.target.value)),
+  fetchedCountries: (country) => dispatch(fetchedCountries(country)),
 });
 
-export default connect(null, mapDispatchToProps)(CountrySelector);
+export default connect(mapStateToProps, mapDispatchToProps)(CountrySelector);
