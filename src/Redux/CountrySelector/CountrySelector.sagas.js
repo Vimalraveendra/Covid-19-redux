38a5e47c82a6594f988ce30fb2d6750391ac7397
vimal Raveendran <vimalraveendra@gmail.com>
2020-05-15 12:24:09 +0200

    Created countrySelector saga and switched asynchrounous action from thunk to redux-saga

---
+++ src/Redux/CountrySelector/CountrySelector.sagas.js
@@ -0,0 +1,28 @@
+import { takeLatest, put, call } from "redux-saga/effects";
+
+import { CountrySelectorActionTypes } from "./CountrySelector.types";
+import { fetchCountryData } from "../../Api/Api";
+
+import {
+  fetchedCountrySuccess,
+  fetchedCountryFailed,
+} from "./CountrySelector.actions";
+
+export function* fetchCountriesStartAsync() {
+  try {
+    // yield keyword is similar to await keyword its intercepts execution.
+    const response = yield call(fetchCountryData);
+    // oR
+    //const response = yield fetchData() avoid using call effects
+    // put affect is used to dispatch actions to reducer
+    put(fetchedCountrySuccess(response));
+  } catch (error) {
+    put(fetchedCountryFailed(error));
+  }
+}
+export function* fetchedCountries() {
+  yield takeLatest(
+    CountrySelectorActionTypes.REQUEST_COUNTRY_DATA_PENDING,
+    fetchCountriesStartAsync
+  );
+}
