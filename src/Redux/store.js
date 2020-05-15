dfb8eacd509e0188bf6dec81341fc24d637cae9d
vimal Raveendran <vimalraveendra@gmail.com>
2020-05-15 12:02:22 +0200

    Updated package dot json with redux-saga for handling asychronous action & applied inside the redux store

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,10 +1,14 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./rootReducer";
 import logger from "redux-logger";
-import thunk from "redux-thunk";
+// import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
 
-const middleWares = [logger, thunk];
+const sagaMiddleware = createSagaMiddleware();
+const middleWares = [logger, sagaMiddleware];
 
 const store = createStore(rootReducer, applyMiddleware(...middleWares));
+
+// sagaMiddleware.run()
 
 export default store;
