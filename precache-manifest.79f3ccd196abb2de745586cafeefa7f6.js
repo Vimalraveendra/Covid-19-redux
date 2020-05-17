dbc1cf70c14d5e41b96ce4bb4bb13ad5db4eafe2
vimal Raveendran <vimalraveendra@gmail.com>
2020-05-17 15:54:19 +0200

    Updates

---
+++ precache-manifest.79f3ccd196abb2de745586cafeefa7f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58fb8e56a77d62d8296ac3dc896d116d",
+    "url": "/Covid-19-redux/index.html"
+  },
+  {
+    "revision": "f346884ff9b7fac83f34",
+    "url": "/Covid-19-redux/static/css/main.9f431cfa.chunk.css"
+  },
+  {
+    "revision": "d830306ccf753be550b2",
+    "url": "/Covid-19-redux/static/js/2.1a98eafa.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "/Covid-19-redux/static/js/2.1a98eafa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f346884ff9b7fac83f34",
+    "url": "/Covid-19-redux/static/js/main.ac2e4cee.chunk.js"
+  },
+  {
+    "revision": "5a610a6b7893407035fd",
+    "url": "/Covid-19-redux/static/js/runtime-main.8f3dc479.js"
+  },
+  {
+    "revision": "13bc297bbd8cb3f1b8b8844540713d6b",
+    "url": "/Covid-19-redux/static/media/Ignis et Glacies Sharp Italic.13bc297b.woff"
+  }
+]);
