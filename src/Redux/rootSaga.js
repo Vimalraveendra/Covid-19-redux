3387eb49744a772e8fad35b34538dd59ef89e53d
vimal Raveendran <vimalraveendra@gmail.com>
2020-05-17 13:52:00 +0200

    updated rootSaga to call all sagas concurrently using the fork affects instead of call affects

--- src/Redux/rootSaga.js
+++ src/Redux/rootSaga.js
@@ -1,7 +1,10 @@
-import { all, call } from "redux-saga/effects";
+import { all, fork } from "redux-saga/effects";
 import { watchFetchData } from "./CovidCard/CovidCard.sagas";
 import { watchFetchCountries } from "./CountrySelector/CountrySelector.sagas";
 
 export function* rootSaga() {
-  yield all[(call(watchFetchData), call(watchFetchCountries))];
+  //Fork performs a non-blocking operation on the function passed.
+  // Call runs a function. If it returns a promise, pauses the saga
+  // until the promise is resolved.
+  yield all([fork(watchFetchData), fork(watchFetchCountries)]);
 }
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -3,16 +3,13 @@ import rootReducer from "./rootReducer";
 import logger from "redux-logger";
 // import thunk from "redux-thunk";
 import createSagaMiddleware from "redux-saga";
-import { watchFetchData } from "./CovidCard/CovidCard.sagas";
-import { watchFetchCountries } from "./CountrySelector/CountrySelector.sagas";
 
-// import {rootSaga} from "./rootSaga"
+import { rootSaga } from "./rootSaga";
 const sagaMiddleware = createSagaMiddleware();
 const middleWares = [sagaMiddleware, logger];
 
 const store = createStore(rootReducer, applyMiddleware(...middleWares));
 
-sagaMiddleware.run(watchFetchCountries);
-sagaMiddleware.run(watchFetchData);
+sagaMiddleware.run(rootSaga);
 
 export default store;
